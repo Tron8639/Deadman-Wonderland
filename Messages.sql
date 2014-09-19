31a42bb65e70686268732ff18a2d62432cb020ff
Tron8639 <herrschmann@web.de>
2014-09-19 22:14:07 +0200

    Create Messages.sql

---
+++ Messages.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `messages` (
+  `uid` int(12) NOT NULL AUTO_INCREMENT,
+  `fromID` varchar(50) NOT NULL,
+  `toID` varchar(50) NOT NULL,
+  `message` text,
+  `fromName` varchar(32) NOT NULL,
+  `toName` varchar(32) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+CREATE DEFINER=`arma3`@`localhost` PROCEDURE `resetLifeVehicles`()
+BEGIN
+	TRUNCATE TABLE `messages`
+END$$
